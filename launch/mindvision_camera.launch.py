17a40203fbdbfc85f553a2b5d4804f30ef3026e4
JellyfishKnight <1002984726@qq.com>
2023-04-07 16:26:37 +0800

    Fix launch and config

--- launch/mindvision_camera.launch.py
+++ launch/mindvision_camera.launch.py
@@ -1,8 +1,23 @@
 import launch
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+import os
 
 def generate_launch_description():
+    params_file = os.path.join(
+        get_package_share_directory('mindvision_camera'), 'config', 'camera_params.yaml')
+
+    camera_info_url = 'package://mindvision_camera/config/camera_info_mv133.yaml'
+    DeclareLaunchArgument(name='params_file',
+                            default_value=params_file),
+    DeclareLaunchArgument(name='camera_info_url',
+                            default_value=camera_info_url),
+    DeclareLaunchArgument(name='use_sensor_data_qos',
+                            default_value='false'),
+
     container = ComposableNodeContainer(
         name='Container',
         namespace='',
@@ -12,7 +27,12 @@ def generate_launch_description():
             ComposableNode(
                 package='mindvision_camera',
                 plugin='RMCamera::MVCamera',
-                name=''
+                name='mindvision_camera_node',
+                parameters=[LaunchConfiguration('params_file'), {
+                'camera_info_url': LaunchConfiguration('camera_info_url'),
+                'use_sensor_data_qos': LaunchConfiguration('use_sensor_data_qos'),
+                }],
+                emulate_tty=True,
             )
         ],
         output='screen'
@@ -20,3 +40,6 @@ def generate_launch_description():
     return launch.LaunchDescription([
         container
         ])
+
+
+
